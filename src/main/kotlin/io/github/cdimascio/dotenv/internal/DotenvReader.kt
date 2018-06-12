b2a3b35bc34252dafc0e92ba910032d9245f98e3
Carmine DiMascio <dimascio@us.ibm.com>
2018-06-12 17:17:45 -0400

    improve error handling

--- src/main/kotlin/io/github/cdimascio/dotenv/internal/DotenvReader.kt
+++ src/main/kotlin/io/github/cdimascio/dotenv/internal/DotenvReader.kt
@@ -1,6 +1,8 @@
 package io.github.cdimascio.dotenv.internal
 
+import io.github.cdimascio.dotenv.DotEnvException
 import java.net.URI
+import java.nio.file.FileSystems
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.stream.Stream
@@ -22,7 +24,15 @@ internal class DotenvReader(
         } else {
             Paths.get(location)
         }
+
         return if (Files.exists(path)) Files.lines(path)
-        else ClasspathHelper.loadFileFromClasspath(location)
+            else try {
+                ClasspathHelper.loadFileFromClasspath(location)
+            } catch (e: DotEnvException) {
+                val cwd = FileSystems.getDefault().getPath(".").toAbsolutePath().normalize()
+                val cwdMessage = if (!path.isAbsolute) "(working directory: $cwd)" else ""
+                e.addSuppressed(DotEnvException("Could not find $path on the file system $cwdMessage"))
+                throw e
+        }
     }
 }
