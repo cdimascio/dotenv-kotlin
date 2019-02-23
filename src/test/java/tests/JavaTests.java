2bda8c2dd2139a34c1be9530b2235622dfc46212
Arniu <arniu2006@gmail.com>
2019-02-23 22:59:27 +0800

    Add more tests

--- src/test/java/tests/JavaTests.java
+++ src/test/java/tests/JavaTests.java
@@ -2,12 +2,25 @@ package tests;
 
 import io.github.cdimascio.dotenv.DotEnvException;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.*;
 
 public class JavaTests {
+    private Map<String, String> envVars;
+
+    @Before
+    public void setUp() {
+        envVars = new HashMap<String, String>();
+        envVars.put("MY_TEST_EV1", "my test ev 1");
+        envVars.put("MY_TEST_EV2", "my test ev 2");
+        envVars.put("WITHOUT_VALUE", "");
+    }
+
     @Test(expected = DotEnvException.class)
     public void throwIfMalconfigured() {
         Dotenv.configure().load();
@@ -16,24 +29,35 @@ public class JavaTests {
     @Test(expected = DotEnvException.class)
     public void load() {
         Dotenv dotenv = Dotenv.load();
-        assertEquals("my test ev 1", dotenv.get("MY_TEST_EV1"));
+
+        for (String envName : envVars.keySet()) {
+            assertEquals(envVars.get(envName), dotenv.get(envName));
+        }
+
+        String envName = "ABSENT_ENV_VARIABLE";
+        String defValue = "This is the default value";
+        assertEquals(defValue, dotenv.get(envName, defValue));
+        assertNull(dotenv.get(envName, defValue));
     }
 
     @Test
     public void configurWithIgnoreMalformed() {
         Dotenv dotenv = Dotenv.configure()
-                .ignoreIfMalformed()
-                .load();
-        assertEquals("my test ev 1", dotenv.get("MY_TEST_EV1"));
+            .ignoreIfMalformed()
+            .load();
+
+        for (String envName : envVars.keySet()) {
+            assertEquals(envVars.get(envName), dotenv.get(envName));
+        }
     }
 
     @Test
     public void configurWithIgnoreMissingAndMalformed() {
         Dotenv dotenv = Dotenv.configure()
-                .directory("/missing/dir")
-                .ignoreIfMalformed()
-                .ignoreIfMissing()
-                .load();
+            .directory("/missing/dir")
+            .ignoreIfMalformed()
+            .ignoreIfMissing()
+            .load();
         assertNotNull(dotenv.get("PATH"));
     }
 }
